4557ffe24fcd665e8a1ee66c0aa85feb56e5a498
浮生凉年 <1009455631@qq.com>
2019-08-20 19:00:22 +0800

    update loggers -> server ..

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+# @Time    : 2019-08-20 16:09
+# @Author  : lidong@immusician.com
+# @Site    :
+# @File    : main.py
+from base_server.server import app
+
+
+if __name__ == '__main__':
+    app.run()
